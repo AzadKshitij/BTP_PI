e1f3c67bc0214c5b443bd8ef4362bce78c1b55b9
Azad Kshitij <azadkshitij08302001@gmail.com>
2022-11-15 15:13:39 +0530

    LogisticRegression

--- server.py
+++ server.py
@@ -1,49 +1,82 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
-# import seaborn as sns
-from scipy import stats
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+import pickle
 
 
 app = Flask(__name__)
 
-model = tf.keras.models.load_model('./mdoels/model_700_r.h5')
-
+filename = 'mdoels\GaussianNB_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 
 @app.route('/')
 def home():
-    return 'Welcome to the home page'
+    return 'Welcome to the home page served by Raspberry Pi'
 
 
-def clear_data(sensor_readings):
-    data = []
+## 4.1
+import math
+def FFT(data):
+    col = data.columns.values
+    df_fft = np.fft.fft(data, axis = 0)
+    abs_df = pd.DataFrame(abs(df_fft), columns= col)
+    
+    return abs_df
 
-    for (load, sensor), g in sensor_readings.groupby(['load', 'sensor']):
-        vals = g.reading.values
-        splits = np.split(vals, range(1000, vals.shape[0], 1000))
-        for i, s in enumerate(splits):
-            data.append({
-                'sensor_a1': int(sensor == 'a1'),
-                'sensor_a2': int(sensor == 'a2'),
-                'sensor_a3': int(sensor == 'a3'),
-                'load': load,
-                'mean': np.mean(s),
-                'std': np.std(s),
-                'kurt': stats.kurtosis(s),
-                'skew': stats.skew(s),
-                'moment': stats.moment(s),
-            })
 
-    df_data = pd.DataFrame(data)
-    # print(df_data)
-    data = df_data.values
-    cols_to_scale = ['load', 'mean', 'std', 'kurt', 'skew', 'moment']
-    scaler = MinMaxScaler()
-    df_data[cols_to_scale] = scaler.fit_transform(df_data[cols_to_scale])
+## 4.2
+def Concate_features(x):
+    index_list = ["S" + str(n) for n in np.arange(1,5)]
+    dff = pd.DataFrame({})
+    for index in index_list:
 
-    return df_data
+        xx = x.loc[[index]]
+        col = [index + "_" + "F" + str(n) for n in np.arange(1,16)]
+        xx.columns = col 
+        xx = xx.reset_index().drop(["index"], axis = 1)
+        dff = pd.concat([dff, xx], axis = 1)
+    return dff
+
+
+## 4.3
+def RMS(data):
+    
+    col = ["F"+ str(n) for n in np.arange(1,16)]
+    
+    x = data[0:10]
+    x = x.applymap(lambda a : a**2)
+    x = pd.DataFrame((x.sum()/10),columns=["F1"]).applymap(lambda a : math.sqrt(a))
+
+    for n in np.arange(1,15):
+        y = data[n*0:(n+1)*10]
+        y = y.applymap(lambda a : a**2)
+        y = pd.DataFrame((y.sum()/10) , columns= [col[n]]).applymap(lambda a : math.sqrt(a))
+        x = pd.concat([x, y], axis = 1)
+    
+    df = Concate_features(x)
+    return df
+
+
+## 4.0
+def Feature_create(data):
+    df = pd.DataFrame({})
+    X = data[['S1', 'S2', 'S3', 'S4']]
+    available_range = X.shape[0]//300
+    if available_range > 0:
+        for n in range(available_range):
+            time_domain = X[n*300:(n+1)*300]
+            freq_domain = FFT(time_domain)
+            f = freq_domain[0:150]
+            f = RMS(f)
+            df = pd.concat([df, f], axis = 0 )
+    
+    else:
+        freq_domain = FFT(X)
+        f = freq_domain
+        df = RMS(f)
+
+
+    return df 
 
 
 @app.route('/predict', methods=['POST'])
@@ -51,17 +84,12 @@ def predict():
     # get the data from the POST request.
     data = request.get_json(force=True)
     df = pd.DataFrame.from_dict(data, orient='index').T
+    df.columns = ['S1', 'S2', 'S3', 'S4','Load']
 
-    sensor_readings = df.melt(
-        id_vars=['load'],
-        value_vars=['a1', 'a2', 'a3', 'a4'],
-        var_name='sensor',
-        value_name='reading'
-    )
-
-    df_data = clear_data(sensor_readings)
+    df_data = Feature_create(df)
+    df["Load"] = [data.get("load")]*df_data.shape[0]
     # make prediction using model loaded from disk as per the data.
-    prediction = model.predict([np.array(list(df_data.values()))])
+    prediction = model.predict(df_data.values)
 
     # Take the first value of prediction
     output = prediction[0]
---
+++ server_1.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+# import seaborn as sns
+from scipy import stats
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+
+app = Flask(__name__)
+
+model = tf.keras.models.load_model('./mdoels/model_700_r.h5')
+
+
+@app.route('/')
+def home():
+    return 'Welcome to the home page'
+
+
+def clear_data(sensor_readings):
+    data = []
+
+    for (load, sensor), g in sensor_readings.groupby(['load', 'sensor']):
+        vals = g.reading.values
+        splits = np.split(vals, range(1000, vals.shape[0], 1000))
+        for i, s in enumerate(splits):
+            data.append({
+                'sensor_a1': int(sensor == 'a1'),
+                'sensor_a2': int(sensor == 'a2'),
+                'sensor_a3': int(sensor == 'a3'),
+                'load': load,
+                'mean': np.mean(s),
+                'std': np.std(s),
+                'kurt': stats.kurtosis(s),
+                'skew': stats.skew(s),
+                'moment': stats.moment(s),
+            })
+
+    df_data = pd.DataFrame(data)
+    # print(df_data)
+    data = df_data.values
+    cols_to_scale = ['load', 'mean', 'std', 'kurt', 'skew', 'moment']
+    scaler = MinMaxScaler()
+    df_data[cols_to_scale] = scaler.fit_transform(df_data[cols_to_scale])
+
+    return df_data
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # get the data from the POST request.
+    data = request.get_json(force=True)
+    df = pd.DataFrame.from_dict(data, orient='index').T
+
+    sensor_readings = df.melt(
+        id_vars=['load'],
+        value_vars=['a1', 'a2', 'a3', 'a4'],
+        var_name='sensor',
+        value_name='reading'
+    )
+
+    df_data = clear_data(sensor_readings)
+    # make prediction using model loaded from disk as per the data.
+    prediction = model.predict(df_data.values)
+
+    # Take the first value of prediction
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    print('Starting Python Flask Server For Load Prediction')
+    app.run(port=5000, debug=True, host='0.0.0.0')
